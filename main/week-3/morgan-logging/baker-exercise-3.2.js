5b794bcea1946a4287f118ec156c05fa61da45b8
Tiffany Baker <tkbaker@my365.bellevue.edu>
2021-01-24 23:02:29 -0600

    added week-3 files

---
+++ main/week-3/morgan-logging/baker-exercise-3.2.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var http = require("http");
+var path =  require("path");
+var logger = require("morgan");
+
+var app = express();
+
+//having express look for files
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+
+app.get("/", function(req, res){
+  res.render("index", {
+    message: "Hello! Welcome and salutations!"
+  });
+});
+
+
+http.createServer(app).listen(3000, function() {
+  console.log("Application started and listening on port %s", 3000);
+});
