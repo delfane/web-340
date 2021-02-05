c50c4b756451268f55b898b427afd7620af3c91a
Tiffany Baker <tkbaker@my365.bellevue.edu>
2021-02-04 20:14:32 -0600

    updated exercise-5.2.js and index.ejs

--- main/week-5/if-else-render/baker-exercise-5.2.js
+++ main/week-5/if-else-render/baker-exercise-5.2.js
@@ -1,32 +1,47 @@
+/*
+
+============================================
+; Title:  exercise-5.2
+; Author: tiffany baker
+; Date:   2/4/2021
+; Modified By: Tiffany Baker
+; Description: a express exercise displaying a if else render in local host.
+;===========================================
+
+*/
+
+
+
+// calling
 var express = require("express");
-
 var http = require("http");
-
 var path = require("path");
 
-app = express();
+//app setup
+
+var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
-
 app.set("view engine", "ejs");
 
-var names = [
+//array for list
+
+var list = [
 
   "Jo Ann",
-
   "Micheal",
-
   "Nick",
-
   "Mary"
 
 ];
 
+//routing app
+
 app.get("/", function(req, res) {
 
-    response.render("index", {
+    res.render("index", {
 
-        names: names
+        names: list
 
     })
 
